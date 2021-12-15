2915d60446d37a742d3562a14ed342a55b6328fb
Opilane TTHK <opilane@tthk.ee>
2021-12-15 16:02:36 +0200

    first commit

---
+++ SantaClausIsComingToTown/Form1.Designer.cs
@@ -0,0 +1,182 @@
+﻿namespace SantaClausIsComingToTown
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.Ground = new System.Windows.Forms.PictureBox();
+            this.Tree = new System.Windows.Forms.PictureBox();
+            this.santa = new System.Windows.Forms.PictureBox();
+            this.flake = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.cchristms = new System.Windows.Forms.Label();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.scorelabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.Ground)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tree)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.santa)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flake)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // Ground
+            // 
+            this.Ground.BackColor = System.Drawing.Color.Transparent;
+            this.Ground.Image = ((System.Drawing.Image)(resources.GetObject("Ground.Image")));
+            this.Ground.Location = new System.Drawing.Point(-1, 513);
+            this.Ground.Name = "Ground";
+            this.Ground.Size = new System.Drawing.Size(556, 75);
+            this.Ground.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Ground.TabIndex = 3;
+            this.Ground.TabStop = false;
+            // 
+            // Tree
+            // 
+            this.Tree.BackColor = System.Drawing.Color.Transparent;
+            this.Tree.Image = ((System.Drawing.Image)(resources.GetObject("Tree.Image")));
+            this.Tree.Location = new System.Drawing.Point(378, 353);
+            this.Tree.Name = "Tree";
+            this.Tree.Size = new System.Drawing.Size(121, 169);
+            this.Tree.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.Tree.TabIndex = 4;
+            this.Tree.TabStop = false;
+            // 
+            // santa
+            // 
+            this.santa.Image = ((System.Drawing.Image)(resources.GetObject("santa.Image")));
+            this.santa.Location = new System.Drawing.Point(-1, 154);
+            this.santa.Name = "santa";
+            this.santa.Size = new System.Drawing.Size(144, 131);
+            this.santa.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.santa.TabIndex = 5;
+            this.santa.TabStop = false;
+            this.santa.Click += new System.EventHandler(this.pictureBox6_Click);
+            // 
+            // flake
+            // 
+            this.flake.Image = ((System.Drawing.Image)(resources.GetObject("flake.Image")));
+            this.flake.Location = new System.Drawing.Point(378, 2);
+            this.flake.Name = "flake";
+            this.flake.Size = new System.Drawing.Size(121, 156);
+            this.flake.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.flake.TabIndex = 6;
+            this.flake.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(-1288, -326);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(38, 15);
+            this.label1.TabIndex = 8;
+            this.label1.Text = "label1";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // cchristms
+            // 
+            this.cchristms.AutoSize = true;
+            this.cchristms.Font = new System.Drawing.Font("Segoe UI", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.cchristms.Location = new System.Drawing.Point(12, 9);
+            this.cchristms.Name = "cchristms";
+            this.cchristms.Size = new System.Drawing.Size(195, 32);
+            this.cchristms.TabIndex = 9;
+            this.cchristms.Text = "Merry Christmas!";
+            // 
+            // timer1
+            // 
+            this.timer1.Enabled = true;
+            this.timer1.Interval = 20;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
+            // 
+            // scorelabel
+            // 
+            this.scorelabel.AutoSize = true;
+            this.scorelabel.Font = new System.Drawing.Font("Segoe UI", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.scorelabel.Location = new System.Drawing.Point(12, 53);
+            this.scorelabel.Name = "scorelabel";
+            this.scorelabel.Size = new System.Drawing.Size(110, 37);
+            this.scorelabel.TabIndex = 10;
+            this.scorelabel.Text = "Score: 0";
+            // 
+            // button1
+            // 
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.Font = new System.Drawing.Font("SimSun", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point);
+            this.button1.Location = new System.Drawing.Point(189, 178);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(157, 130);
+            this.button1.TabIndex = 11;
+            this.button1.Text = "PLAY AGAIN";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Visible = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(556, 587);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.scorelabel);
+            this.Controls.Add(this.cchristms);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.flake);
+            this.Controls.Add(this.santa);
+            this.Controls.Add(this.Tree);
+            this.Controls.Add(this.Ground);
+            this.Name = "Form1";
+            this.Text = "    ";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyDown);
+            this.KeyUp += new System.Windows.Forms.KeyEventHandler(this.Form1_KeyUp);
+            ((System.ComponentModel.ISupportInitialize)(this.Ground)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Tree)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.santa)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.flake)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox Tree;
+        private System.Windows.Forms.PictureBox santa;
+        private System.Windows.Forms.PictureBox flake;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label cchristms;
+        private System.Windows.Forms.PictureBox Ground;
+        private System.Windows.Forms.Timer timer1;
+        private System.Windows.Forms.Label scorelabel;
+        private System.Windows.Forms.Label score;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ SantaClausIsComingToTown/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace SantaClausIsComingToTown
+{
+    public partial class Form1 : Form
+    {
+        int gravity = 10;
+        int objectspeed = 6;
+        int gameScore = 0;
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void pictureBox6_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            santa.Top += gravity;
+            flake.Left -= objectspeed;
+            Tree.Left -= objectspeed;
+            scorelabel.Text = $"Score : {gameScore}";   
+
+            if(flake.Left < -120)
+            {
+                flake.Left = 600;
+                 
+            }
+            if(Tree.Left < -120)
+            {
+                Tree.Left = 600;
+                gameScore++;
+            }
+
+            if (santa.Top < -10)
+            {
+                gameover();
+            }
+
+            if(santa.Bounds.IntersectsWith(flake.Bounds) || santa.Bounds.IntersectsWith(Tree.Bounds) || santa.Bounds.IntersectsWith(Ground.Bounds))
+            {
+                gameover();
+            }
+        }
+
+        private void Form1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                gravity = -3;
+            }
+        }
+
+        private void Form1_KeyUp(object sender, KeyEventArgs e)
+        {
+            if(e.KeyCode == Keys.Space)
+            {
+                gravity = 3;
+            }
+        }
+
+        private void gameover()
+        {
+            timer1.Stop();
+            scorelabel.Text = $"Game Over!";
+            button1.Visible = true;
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form1 newform = new Form1();
+            newform.Show();
+            this.Dispose(false);
+        }
+    }
+}
